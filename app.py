1964ee34b866cd786f3b90418407cb50c7f56f78
IDOWUMAYOWA <sangotadeidowu@yahoo.com>
2025-12-29 02:04:51 +0100

    This commit is for flask app and html update

--- app.py
+++ app.py
@@ -14,15 +14,26 @@ scaler = pickle.load(open('/home/ims24/housing/scaler.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/predict_test', methods=['POST'])
+def predict_test():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
     tranform_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    prediction = reg_model.predict(tranform_data)
+    prediction = reg_model.predictt(tranform_data)
     print(prediction[0])
     return jsonify(prediction[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    prediction = reg_model.predict(final_input)[0]
+    return render_template('home.html', prediction_text=f'The predicted housing price is {prediction}')
+   
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
